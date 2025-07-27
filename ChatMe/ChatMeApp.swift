0c23a588640ab7bf74fa542a1832ab3cce1cce9f
Opshanka <opibabeykoon@gmail.com>
2025-07-27 10:34:56 +0530

    Initial Commit

---
+++ ChatMe/ChatMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMeApp.swift
+//  ChatMe
+//
+//  Created by Opshanka Prabath on 2025-07-27.
+//
+
+import SwiftUI
+
+@main
+struct ChatMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
